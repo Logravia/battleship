adef7ed0b85a40ae7d01b8a1130fc2fcb59432b5
Jānis Alunāns <j.alunans@gmail.com>
2022-12-21 20:34:00 +0200

    Add letters, numbers to grid

--- src/display.js
+++ src/display.js
@@ -13,7 +13,7 @@ export class Display {
 
   update(state) {
     this.gridButtons.forEach(btn=>{
-      btn.textContent = state[btn.dataset.y][btn.dataset.x];
+      btn.className = `${state[btn.dataset.y][btn.dataset.x]} grid-btn`
     });
   }
 
@@ -28,9 +28,11 @@ export class Display {
   #makeShip(len) {
     let ship = document.createElement("div");
     ship.className = "ship";
+    ship.dataset.len = len;
+    ship.dataset.dir = "vertical"
 
     for (let i = 0; i < len; i++) {
-      ship.innerHTML += '<div class="ship-section" data-vertical="true" data-len="${len}"></div>'
+      ship.innerHTML += '<div class="ship-section"></div>'
     }
 
     return ship
@@ -48,6 +50,8 @@ export class Display {
   #frame() {
     let frame = document.createElement("div");
     frame.className = "frame"
+    frame.appendChild(this.#letters());
+
 
     for (let y = 0; y < helper.HEIGHT; y++) {
       frame.appendChild(this.#newRow(y))
@@ -56,15 +60,40 @@ export class Display {
     return frame;
   }
 
+  #letters () {
+    let letters = document.createElement("div")
+    letters.className = "rowDiv"
+    letters.innerHTML = `
+    <button class="letter"></button>
+    <button class="letter">A</button>
+    <button class="letter">B</button>
+    <button class="letter">C</button>
+    <button class="letter">D</button>
+    <button class="letter">E</button>
+    <button class="letter">F</button>
+    <button class="letter">G</button>
+    <button class="letter">H</button>
+    <button class="letter">J</button>
+    <button class="letter">K</button>
+    `
+    return letters;
+  }
+
   #newRow(y){
     let rowDiv = document.createElement("div");
     rowDiv.className = "rowDiv";
+    let number = document.createElement("button")
+    number.className = "number"
+
+    number.textContent = y;
+
+    rowDiv.appendChild(number);
 
     for (let x = 0; x < helper.WIDTH; x++){
       let btn = document.createElement("button")
       btn.dataset.x = x
       btn.dataset.y = y
-      btn.class = "grid-btn"
+      btn.className = "grid-btn"
       rowDiv.appendChild(btn);
       this.gridButtons.push(btn);
     }
