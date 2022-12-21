b2a73be2dff7393a948498b46ab5470fddec7400
Jānis Alunāns <j.alunans@gmail.com>
2022-12-21 20:35:14 +0200

    Fix ship placement bug in listener

--- src/listeners.js
+++ src/listeners.js
@@ -1,11 +1,13 @@
 import {Board} from "./board.js"
 import {Display} from "./display.js"
+import { Ship } from "./ship.js";
 
 export class Listeners {
   constructor (board = new Board(), display = new Display()) {
     this.board = board;
     this.display = display;
-    this.btns = display.gridButtons
+    this.btns = display.gridButtons;
+    this.ships = display.ships;
     this.selectedShip = undefined;
   }
 
@@ -22,24 +24,39 @@ export class Listeners {
 
   // Removes all listeners
   disabled() {
-
+    display.reset();
   }
 
   placementListeners () {
+    // for grid buttons
     this.btns.forEach(btn=>{
       // Allow click to place the ship
       btn.addEventListener("click", e=>{
         let sqr = e.currentTarget;
-        let coords = {x: sqr.dataset.x, y: sqr.dataset.y}
+        let coords = {x: Number(sqr.dataset.x), y: Number(sqr.dataset.y)}
         this.#putShip(coords);
       })
       //Highlight where ship will be placed based on cursor, shipSize
       btn.addEventListener("mouseenter", e=>{
-        this.display.highlightShipArea(e.currentTarget, this.selectedShip)
+        if (this.selectedShip) {
+          this.display.highlightShipArea(e.currentTarget, this.selectedShip)
+        }
       })
       // Remove highlight
       btn.addEventListener("mouseleave", e=>{
-        this.display.removeShipHighlight(e.target, this.selectedShip)
+        if(this.selectedShip){
+          this.display.removeShipHighlight(e.target, this.selectedShip)
+        }
+      })
+    })
+    //for ships to be placed
+    this.ships.forEach(ship=>{
+      ship.addEventListener("click", e=>{
+        this.selectedShip = e.currentTarget;
+      })
+
+      ship.addEventListener("dblclick", e=>{
+        this.display.rotateShip(e.currentTarget);
       })
     })
   }
@@ -68,10 +85,23 @@ export class Listeners {
     this.display.update(this.board.overview());
   }
 
+  makeShip() {
+    let len = Number(this.selectedShip.dataset.len)
+    let dir = this.selectedShip.dataset.dir;
+    let ship = new Ship(len, dir);
+    return ship
+  }
+
   #putShip (coords) {
     if (this.selectedShip) {
-      this.board.addShip(coords, this.selectedShip);
+
+      let ship = this.makeShip();
+
+      if(!this.board.legalSpotForShip(coords, ship)){return}
+
+      this.board.addShip(coords, ship);
       this.display.update(this.board.overview())
+      this.selectedShip.remove()
       this.selectedShip = undefined;
     }
   }
